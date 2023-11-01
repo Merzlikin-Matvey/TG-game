a19b1dc85887e2dbc1a122548f05037922c91a72
Matvey Merzlikin <merzlikinmatvey01@gmail.com>
2023-11-01 20:35:40 +0300

    Попытка добавить 1 день

--- python/firstWeek.py
+++ python/firstWeek.py
@@ -1,5 +1,42 @@
 import telebot
+import os
+from dotenv import load_dotenv
+import time
 
-def day1(bot: telebot.Telebot, message):
-    bot.send_message(message.chat.id, '9:00. 229 кабинет, алгебра')
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+bot = telebot.TeleBot(TOKEN)
+
+def startWeek1(message):
+    bot.register_next_step_handler(message, f1)
+
+
+
+def f1(message):
+    bot.send_message(message.chat.id, '8:50. 229 кабинет, алгебра')
+    time.sleep(1.5)
     
+    keyboard = telebot.types.ReplyKeyboardMarkup()
+    key_yes = telebot.types.KeyboardButton(text="Да, делал")
+    key_probably = telebot.types.KeyboardButton(text="Возможно частично")
+    key_no = telebot.types.KeyboardButton(text="Нет, я не успел")
+
+    keyboard.add(key_yes, key_probably, key_no)
+    bot.send_message(message.chat.id, 'Иван: Ты домашку делал?', reply_markup=keyboard)                                                                                                                               
+    bot.register_next_step_handler(message, f2)
+
+def f2(message):
+    print(message.text)
+    bot.send_message(message.chat.id, 'я здеся')
+    if message.text == "Да, делал":
+        bot.send_message(message.chat.id, "Иван: Отлично, просто я не успел. Прикрой меня")
+        time.sleep(1)
+        bot.send_message(message.chat.id, "10 минут спустя")
+        time.sleep(1)
+        bot.send_message(message.chat.id, "Дмитрий Евгеньевич: Так, кто сделал домашку?")
+        
+        keyboard = telebot.types.InlineKeyboardMarkup()
+        key_queit = telebot.types.InlineKeyboardButton(text="Промолчу..", callback_data="/Промолчу")
+        key_say = telebot.types.InlineKeyboardButton(text="Я отвечу, зачем молчать?", callback_data="/Скажу")
+    else:
+        bot.send_message(message.chat.id, message.text)
--- python/start.py
+++ python/start.py
@@ -38,6 +38,7 @@ def get_name(message):
     keyboard.add(key_botan, key_sportsman, key_poet, key_person)
 
     bot.send_message(message.chat.id, 'Теперь выбери, за какой класс хочешь играть', reply_markup=keyboard)
+
     bot.register_next_step_handler(message, get_class)
 
 
@@ -62,12 +63,47 @@ def get_class(message):
 
 def get_group(message):
     if message.text == '1':
-        bot.send_message(message.chat.id, 'Первая группа. А других вариантов и быть не могло!')
+        bot.send_message(message.chat.id, 
+                         'Первая группа. А других вариантов и быть не могло!')
+        startWeek1(message)
     elif message.text in list(map(str, [i for i in range(2, 6 + 1)])):
         bot.send_message(message.chat.id, 'Фатальная ошибка. Вы проиграли')
 
 
 
+def startWeek1(message):
+    bot.register_next_step_handler(message, f1)
+
+
+
+def f1(message):
+    bot.send_message(message.chat.id, '8:50. 229 кабинет, алгебра')
+    time.sleep(1.5)
+    
+    keyboard = telebot.types.ReplyKeyboardMarkup()
+    key_yes = telebot.types.KeyboardButton(text="Да, делал")
+    key_probably = telebot.types.KeyboardButton(text="Возможно частично")
+    key_no = telebot.types.KeyboardButton(text="Нет, я не успел")
+
+    keyboard.add(key_yes, key_probably, key_no)
+    bot.send_message(message.chat.id, 'Иван: Ты домашку делал?', reply_markup=keyboard)                                                                                                                               
+    bot.register_next_step_handler(message, f2)
+
+def f2(message):
+    print(message.text)
+    bot.send_message(message.chat.id, 'я здеся')
+    if message.text == "Да, делал":
+        bot.send_message(message.chat.id, "Иван: Отлично, просто я не успел. Прикрой меня")
+        time.sleep(1)
+        bot.send_message(message.chat.id, "10 минут спустя")
+        time.sleep(1)
+        bot.send_message(message.chat.id, "Дмитрий Евгеньевич: Так, кто сделал домашку?")
+        
+        keyboard = telebot.types.InlineKeyboardMarkup()
+        key_queit = telebot.types.InlineKeyboardButton(text="Промолчу..", callback_data="/Промолчу")
+        key_say = telebot.types.InlineKeyboardButton(text="Я отвечу, зачем молчать?", callback_data="/Скажу")
+    else:
+        bot.send_message(message.chat.id, message.text)
 
 
 
