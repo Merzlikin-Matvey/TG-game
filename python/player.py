9bd726c12c8389ecc6f2ba07a6503c5d9d63ec85
Merzlikin-Matvey <merzlikinmatvey01@gmail.com>
2023-11-02 00:56:36 +0300

    Половина первого дня (ну хотя б работает)

--- python/player.py
+++ python/player.py
@@ -8,22 +8,30 @@ PLAYER_CLASSES = {
 
 
 class Player:
-    def __init__(self, player_class, player_name):
+    def __init__(self, player_name, player_class):
         self.player_class = player_class
         self.player_name = player_name
         self.params = PLAYER_CLASSES[player_class]
 
     def intelligence(self, delta):
         self.params[0] += delta
+        return (f'Ваш интеллект изменен на {delta} \n'
+                f'Новое значение  {self.params[0]}')
 
     def health(self, delta):
         self.params[1] += delta
+        return (f'Ваше здоровье изменено на {delta} \n'
+                f'Новое значение  {self.params[1]}')
 
     def eloquence(self, delta):
         self.params[2] += delta
+        return (f'Ваше красноречие изменено на {delta} \n'
+                f'Новое значение  {self.params[2]}')
 
     def rep(self, delta):
         self.params[3] += delta
+        return (f'Ваша репутация изменена на {delta} \n'
+                f'Новое значение  {self.params[3]}')
 
 
 
--- python/start.py
+++ python/start.py
@@ -1,15 +1,25 @@
-import time
-import telebot
-import os
-from dotenv import load_dotenv
+'''
+Я всю игру сделал в одном файле так как не смог адекватно разделить это на файлы
+Проблема с bot.register_next_step_handler
+Если найдется человек, который сможет мне помочь, буду благодарен ☺
+'''
 
+import time
+import os
+
+from dotenv import load_dotenv
+import telebot
+import numpy as np
+
+from player import Player
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 bot = telebot.TeleBot(TOKEN)
 
-player_name = None
-player_class = None
+player_name = 'Аллллллексей'
+player_class = 'Ботан'
+player = Player('Аллллллексей', 'Ботан')
 
 
 @bot.message_handler(commands=['start'])
@@ -25,9 +35,8 @@ def start(message):
     bot.register_next_step_handler(message, get_name)
 
 
-
 def get_name(message):
-    global player_name
+    global player, player_name, player_class
     player_name = message.text
 
     keyboard = telebot.types.ReplyKeyboardMarkup()
@@ -42,9 +51,8 @@ def get_name(message):
     bot.register_next_step_handler(message, get_class)
 
 
-
 def get_class(message):
-    global player_class, player_name
+    global player, player_name, player_class
     player_class = message.text
 
     send = f'{player_name}, {player_class.lower()} это отличный выбор!'
@@ -62,50 +70,237 @@ def get_class(message):
 
 
 def get_group(message):
+    global player, player_name, player_class
+    player = Player(player_name, player_class)
     if message.text == '1':
-        bot.send_message(message.chat.id, 
-                         'Первая группа. А других вариантов и быть не могло!')
-        startWeek1(message)
+        bot.send_message(message.chat.id,
+                         'Первая группа. А других вариантов и быть не могло!',
+                         reply_markup=telebot.types.ReplyKeyboardRemove())
+        f1(message)
     elif message.text in list(map(str, [i for i in range(2, 6 + 1)])):
-        bot.send_message(message.chat.id, 'Фатальная ошибка. Вы проиграли')
-
-
-
-def startWeek1(message):
-    bot.register_next_step_handler(message, f1)
-
+        bot.send_message(message.chat.id, 'Фатальная ошибка. Вы проиграли',
+                         reply_markup=telebot.types.ReplyKeyboardRemove())
 
 
 def f1(message):
+    time.sleep(1)
     bot.send_message(message.chat.id, '8:50. 229 кабинет, алгебра')
     time.sleep(1.5)
-    
+
     keyboard = telebot.types.ReplyKeyboardMarkup()
     key_yes = telebot.types.KeyboardButton(text="Да, делал")
     key_probably = telebot.types.KeyboardButton(text="Возможно частично")
     key_no = telebot.types.KeyboardButton(text="Нет, я не успел")
 
     keyboard.add(key_yes, key_probably, key_no)
-    bot.send_message(message.chat.id, 'Иван: Ты домашку делал?', reply_markup=keyboard)                                                                                                                               
+    bot.send_message(message.chat.id, 'Иван: Ты домашку делал?', reply_markup=keyboard)
     bot.register_next_step_handler(message, f2)
 
+
 def f2(message):
-    print(message.text)
-    bot.send_message(message.chat.id, 'я здеся')
     if message.text == "Да, делал":
-        bot.send_message(message.chat.id, "Иван: Отлично, просто я не успел. Прикрой меня")
+        bot.send_message(message.chat.id, "Иван: Отлично, просто я не успел. Прикрой меня",
+                         reply_markup=telebot.types.ReplyKeyboardRemove())
         time.sleep(1)
         bot.send_message(message.chat.id, "10 минут спустя")
         time.sleep(1)
-        bot.send_message(message.chat.id, "Дмитрий Евгеньевич: Так, кто сделал домашку?")
-        
+
         keyboard = telebot.types.InlineKeyboardMarkup()
-        key_queit = telebot.types.InlineKeyboardButton(text="Промолчу..", callback_data="/Промолчу")
+        key_quiet = telebot.types.InlineKeyboardButton(text="Промолчу..", callback_data="/Промолчу")
         key_say = telebot.types.InlineKeyboardButton(text="Я отвечу, зачем молчать?", callback_data="/Скажу")
+
+        keyboard.add(key_quiet, key_say)
+        bot.send_message(message.chat.id, "Дмитрий Евгеньевич: Так, кто сделал домашку?", reply_markup=keyboard)
+
+        @bot.callback_query_handler(func=lambda call: True)
+        def handle_callback_query(call):
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                  text="Дмитрий Евгеньевич: Так, кто сделал домашку?", reply_markup=None)
+
+            if call.data == '/Промолчу':
+                time.sleep(2)
+                bot.send_message(call.message.chat.id, "Дмитрий Евгеньевич: Лес рук...")
+                time.sleep(1)
+                bot.send_message(call.message.chat.id, "Дмитрий Евгеньевич: Тогда пойдем по списку")
+                time.sleep(1)
+                bot.send_message(call.message.chat.id, "Дмитрий Евгеньевич: К доске пойдет..")
+                time.sleep(1)
+                bot.send_message(call.message.chat.id, "Дмитрий Евгеньевич: К доске пойдет...")
+                time.sleep(1)
+                bot.send_message(call.message.chat.id, "Дмитрий Евгеньевич: К доске пойдет....")
+
+                loser = np.random.choice(np.array(
+                    [player.player_name, player.player_name, "Иван"]))
+                bot.send_message(call.message.chat.id, loser)
+
+                if loser == player.player_name:
+                    time.sleep(1)
+                    bot.send_message(call.message.chat.id,
+                                     f' Дмитрий Евгеньевич:{player.player_name}, решите это уравнение')
+                    photo = open('../img/cubic_equation.jpg', 'rb')
+                    bot.send_photo(call.message.chat.id, photo=photo)
+
+                    def f_2_1(message):
+                        try:
+                            ans = sorted(list(map(int, message.text.split())))
+                        except:
+                            ans = -1
+                        if ans == [57, 58, 239]:
+                            bot.send_message(message.chat.id, "Дмитрий Евгеньевич: Фантастика! Верно!")
+                            time.sleep(1)
+                            bot.send_message(message.chat.id, player.intelligence(5))
+                            player.rep(10)
+                        else:
+                            bot.send_message(message.chat.id, 'Дмитрий Евгеньевич: Неправильно! Садитесь, 2!')
+                            bot.send_message(message.chat.id, player.intelligence(-5))
+                            player.rep(-10)
+                            time.sleep(2)
+                            bot.send_message(message.chat.id, "Дмитрий Евгеньевич: Иван, а вы сделали этот номер?")
+                            time.sleep(2)
+                            bot.send_message(message.chat.id, "Иван: Нет, я не успел")
+                            time.sleep(2)
+                            bot.send_message(message.chat.id, "Дмитрий Евгеньевич: не успевают только неуспевающие!")
+                            time.sleep(2)
+                            bot.send_message(message.chat.id, "Через час")
+                            time.sleep(2)
+
+                            keyboard = telebot.types.ReplyKeyboardMarkup()
+                            key_sorry = telebot.types.KeyboardButton(text="Прости, ошибся")
+                            key_lol = telebot.types.KeyboardButton(text="Я пошутил :D")
+                            keyboard.add(key_sorry, key_lol)
+
+                            bot.send_message(message.chat.id,
+                                             f"Иван: Блин, {player.player_name}, ты же сказал что сделал домашку",
+                                             reply_markup=keyboard)
+
+                            def f_2_1_1(message):
+                                if message.text == "Прости, ошибся":
+                                    bot.send_message(message.chat.id, "Иван: Да ладно, бывает",
+                                                     reply_markup=telebot.types.ReplyKeyboardRemove())
+                                else:
+                                    bot.send_message(message.chat.id, "Иван: Друг, называется",
+                                                     reply_markup=telebot.types.ReplyKeyboardRemove())
+
+                            bot.register_next_step_handler(message, f_2_1_1)
+
+                    bot.register_next_step_handler(message, f_2_1)
+
+                else:
+                    bot.send_message(message.chat.id, "Иван: Я не успел сделать домашку")
+                    time.sleep(2)
+                    bot.send_message(message.chat.id, "Дмитрий Евгеньевич: не успевают только неуспевающие!")
+                    time.sleep(2)
+                    bot.send_message(message.chat.id, "Через час")
+                    time.sleep(2)
+
+                    keyboard = telebot.types.ReplyKeyboardMarkup()
+                    key_bye = telebot.types.KeyboardButton(text="Пока")
+                    keyboard.add(key_bye)
+
+                    bot.send_message(message.chat.id, "Иван: Не повезло мне сегодня. Ну ладно, пока",
+                                     reply_markup=keyboard)
+
+                    def f_2_2(message):
+                        pass
+
+                    bot.register_next_step_handler(message, f_2_2)
+
+            elif call.data == '/Скажу':
+                bot.send_message(call.message.chat.id, f'Дмитрий Евгеньевич: О, {player.player_name}, вы сделали')
+                time.sleep(2)
+                photo = open('../img/cubic_equation.jpg', 'rb')
+                bot.send_photo(call.message.chat.id, photo=photo)
+                time.sleep(1)
+                bot.send_message(call.message.chat.id, f'Дмитрий Евгеньевич: Тогда решите это уравнение')
+
+                def f_2_3(message):
+                    try:
+                        ans = sorted(list(map(int, message.text.split())))
+                    except:
+                        ans = -1
+                    if ans == [57, 58, 239]:
+                        bot.send_message(message.chat.id, "Дмитрий Евгеньевич: Замечательно. Все правильно")
+                        time.sleep(1)
+                        bot.send_message(message.chat.id, player.intelligence(5))
+                        player.rep(10)
+                    else:
+                        bot.send_message(message.chat.id, 'Дмитрий Евгеньевич: Неправильно! Садитесь, 2!')
+                        bot.send_message(message.chat.id, player.intelligence(-5))
+                        player.rep(-10)
+                    time.sleep(2)
+                    bot.send_message(message.chat.id, 'Через час')
+
+                bot.register_next_step_handler(message, f_2_3)
+
+
+def f3(message):
+    if message.text == "Возможно частично":
+        bot.send_message(message.chat.id, "Иван: Ха-ха, понял",
+                         reply_markup=telebot.types.ReplyKeyboardRemove())
+        time.sleep(1)
+        bot.send_message(message.chat.id, "10 минут спустя")
+        time.sleep(1)
+
+        keyboard = telebot.types.ReplyKeyboardMarkup()
+        key_probably = telebot.types.KeyboardButton(text="Я только часть дз сделал")
+        key_yes = telebot.types.KeyboardButton(text="Да")
+        keyboard.add(key_probably, key_yes)
+
+        bot.send_message(message.chat.id, f"Дмитрий Евгеньевич: {player.player_name}, вы сделали номер 09.10?")
+
+        def f_3_1(message):
+            if message.text == "Я только часть дз сделал":
+                bot.send_message(message.chat.id,
+                                 f"Дмитрий Евгеньевич: Тогда покажите Александре Игоревне то, что сделали")
+                time.sleep(2)
+                bot.send_message(message.chat.id, "15 минут спустя")
+                bot.send_message(message.chat.id,
+                                 f"Александра Игоревна: В том, что вы сделали, ошибок нет")
+            if message.text == "Да":
+                bot.send_message(message.chat.id,
+                                 f"Дмитрий Евгеньевич: Тогда покажите Александре Игоревне тетрадь")
+
+                keyboard = telebot.types.ReplyKeyboardMarkup()
+                key_forgot = telebot.types.KeyboardButton(text="Я дома забыл")
+                key_no = telebot.types.KeyboardButton(text="Я не успел его сделать")
+                keyboard.add(key_forgot, key_no)
+
+                time.sleep(2)
+                bot.send_message(message.chat.id, "Александра Игоревна: Так, а где номер 09.10?",
+                                 reply_markup=keyboard)
+
+                def f_3_1_1(message):
+                    if message.text == "Я дома забыл":
+                        if player.params[2] > 0:
+                            bot.send_message(message.chat.id, ("У вас хорошо прокачено красноречие!"
+                                                               "Вы убедили Александру Игоревну, что забыли задание дома"))
+                            bot.send_message(message.chat.id, player.eloquence(5))
+                        else:
+                            bot.send_message(message.chat.id, "Александра Игоревна: Не надо врать!")
+                            bot.send_message(message.chat.id, player.health(-5))
+                    else:
+                        bot.send_message(message.chat.id, "Александра Игоревна: Ладно, в пятницу принесете")
+
+                bot.register_next_step_handler(message, f_3_1_1)
+                time.sleep(1)
+                bot.send_message(message.chat.id, 'Через час')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     else:
         bot.send_message(message.chat.id, message.text)
-
-
 
 
 if __name__ == '__main__':
